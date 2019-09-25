08b71b3f839c60bae0508ac135b05d58ac9ce95a
agnuss7 <agne.burokaite@gmail.com>
2019-09-26 01:17:39 +0300

    programos v0.1 beveik baigta, reikia readme padaryt

--- hash.c
+++ hash.c
@@ -4,7 +4,9 @@
 #include<stdio.h>
 #include<cmath>
 #include<iostream>
-#include <bitset>
+#include<bitset>
+#include<vector>
+
 
 std::string readfrom(std::string name)
 {
@@ -15,28 +17,62 @@ std::string readfrom(std::string name)
     return out;
 }
 
-std::string hash(std::string a) {
+std::string base="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+std::string hash(std::string a) {
+std::vector<bool> bin;
+int nn=0;
+for (char x : a)
+{
+for(int i=0;i<8;i++)
+{
+bin.push_back(std::bitset<8>((int)x)[i]);
+}
+nn+=(int)x;
+}
+while(bin.size()/6<20)
+{
+bin.push_back(0);
+bin.push_back(1);
+}
+srand(nn);
+for(int i=0;i<bin.size();i++)
+{
+bin[i]=!bin[i];
+int j=rand()%bin.size();
+bin[j]=!bin[j];
+}
+int ll=(bin.size()-20*6)/20;
+std::string out="";
+std::bitset<6> letter;
+for(int i=0;i<bin.size();i+=6+ll)
+{
+for(int o=0;o<6;o++)
+{
+letter[o]=bin[i+o];
+}
+out+=base[letter.to_ulong()];
+}
+return out;
+//
 }
 
 int main () {
+do{
     bool boo;
     printf("ivesit string - 0, is failo - 1?\n");
     std::cin>>boo;
     if(boo==0) {
     std::string v;
     std::cin>>v;
-    printf("kas isejo: %s\n",v.c_str());
-
+    printf("kas isejo: %s\nbinary: %s\n",v.c_str(),hash(v).c_str());
     }
     else {
-        std::string bin="";
-        for (char x : v)
-        {bin+=std::bitset<8>((int)x).to_string();}
-        printf("kas iejo: %s\nbinary:%s\n",hash(readfrom("a.txt")).c_str(),bin);
-
+	std::string v=readfrom("a.txt");
+        printf("kas iejo: %s\nbinary:%s\n",v.c_str(),hash(v).c_str());
+	
     }
-
+} while(1);
 
 
 
