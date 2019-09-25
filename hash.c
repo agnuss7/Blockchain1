d011c993db0512dec2451a3068789dc51a0a1915
agne burokaite <agne.burokaite@gmail.com>
2019-09-25 17:34:45 +0300

    3 vis dar tik isbandau

--- hash.c
+++ hash.c
@@ -4,6 +4,7 @@
 #include<stdio.h>
 #include<cmath>
 #include<iostream>
+#include <bitset>
 
 std::string readfrom(std::string name)
 {
@@ -19,22 +20,25 @@ std::string hash(std::string a) {
 }
 
 int main () {
-bool boo;
-printf("ivesit string - 0, is failo - 1?\n");
-std::cin>>boo;
-if(boo==0) {
-std::string v;
-std::cin>>v;
-printf("kas isejo: %s\n",v.c_str());
+    bool boo;
+    printf("ivesit string - 0, is failo - 1?\n");
+    std::cin>>boo;
+    if(boo==0) {
+    std::string v;
+    std::cin>>v;
+    printf("kas isejo: %s\n",v.c_str());
 
-}
-else {
-printf("kas isejo: %s\n",hash(readfrom("a.txt")).c_str());
+    }
+    else {
+        std::string bin="";
+        for (char x : v)
+        {bin+=std::bitset<8>((int)x).to_string();}
+        printf("kas iejo: %s\nbinary:%s\n",hash(readfrom("a.txt")).c_str(),bin);
 
-}
+    }
 
 
 
 
-return 0;
+    return 0;
 }
